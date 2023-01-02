2e2f052ae6b3f6ae18e11b3a2b66896b1acaf6af
Jesus Berisa Nuñez <33457979+llius123@users.noreply.github.com>
2023-01-02 11:20:10 +0100

    refactor(2023): way to create excel

--- index.js
+++ index.js
@@ -3,22 +3,10 @@ const { XMLParser } = require("fast-xml-parser");
 
 const allTables = getDataFromBoe();
 
-const parser = new XMLParser(options);
-let jsonObj = parser.parse(data);
-const allTables = jsonObj.documento.texto.table;
-
-//Create excel
-
-var xl = require("excel4node");
-
-// Create a new instance of a Workbook class
-var wb = new xl.Workbook();
-
-// Add Worksheets to the workbook
-var ws = wb.addWorksheet("Coches");
+const { excel, excelPage } = createAndConfigureExcel();
 
 let row = 1;
-createHeader(ws);
+createHeader(excelPage);
 row++;
 for (const table of allTables) {
   if (table.thead.tr[0] === undefined) {
@@ -26,9 +14,9 @@ for (const table of allTables) {
   }
   const marca = table.thead.tr[0].th["#text"].replace(/Marca: /g, "");
 
-  createBody(marca, table.tbody.tr, ws);
+  createBody(marca, table.tbody.tr, excelPage);
 }
-wb.write("boe.xlsx");
+excel.write("boe.xlsx");
 
 function createHeader(test) {
   const currentWS = test;
@@ -133,4 +121,14 @@ function getDataFromBoe() {
   }
 }
 
-function createAndConfigureExcel() {}
+function createAndConfigureExcel() {
+  //Create excel
+  var xl = require("excel4node");
+
+  // Create a new instance of a Workbook class
+  var excel = new xl.Workbook();
+
+  // Add Worksheets to the workbook
+  var excelPage = excel.addWorksheet("Coches");
+  return { excel, excelPage };
+}
